bf0c7ff4f503ae9ca26ab6adafd1221b65dcd811
Stephen Muema <muema1476@gmail.com>
2022-08-31 09:38:46 +0300

    Added button function

--- app/src/main/java/com/example/myfoodapp/Homepage.kt
+++ app/src/main/java/com/example/myfoodapp/Homepage.kt
@@ -1,11 +1,24 @@
 package com.example.myfoodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val steveButton=findViewById<Button>(R.id.steveButton)
+        steveButton.setOnClickListener{
+            var Intent=Intent(this,MainActivity::class.java)
+        startActivity(Intent)
+            finish()
+        }
+
+
     }
 }
